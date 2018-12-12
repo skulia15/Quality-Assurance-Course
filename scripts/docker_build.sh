e3e51e3cbb86f14e0eda9a40779d1ab81edf2816
Skuli Arnarsson <skulia15@ru.is>
2018-12-12 17:58:05 +0000

    add api and client to same docker repo

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -4,7 +4,6 @@
 GIT_COMMIT=$1
 
 cd game-api || exit 1
-docker build -t skulipardus/hgop:$GIT_COMMIT . || exit 1
+docker build -t skulipardus/hgop-api:$GIT_COMMIT . || exit 1
 cd ../game-client || exit 1
-# docker build . -t skulipardus/lucky21-front-end:$GIT_COMMIT || exit 1
-docker build . -t skulipardus/lucky21-front-end:$GIT_COMMIT || exit 1
+docker build . -t skulipardus/hgop-lucky21-front-end:$GIT_COMMIT || exit 1
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -2,5 +2,5 @@
 
 GIT_COMMIT=$1
 
-docker push skulipardus/hgop:$GIT_COMMIT || exit 1
-docker push skulipardus/lucky21-front-end:$GIT_COMMIT || exit 1
+docker push skulipardus/hgop-api:$GIT_COMMIT || exit 1
+docker push skulipardus/hgop-lucky21-front-end:$GIT_COMMIT || exit 1
