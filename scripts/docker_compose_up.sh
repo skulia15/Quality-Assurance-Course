0a1cc08b465aa665d061e88adee6c7377ac89e59
Skuli Arnarsson <skulia15@ru.is>
2018-12-12 18:02:55 +0000

    api_url not hardcoded

--- scripts/docker_compose_up.sh
+++ scripts/docker_compose_up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export GIT_COMMIT=$1
-# LALALALA_API_LALALAL_URL="http//:localhost:3000"
-# export ENVIRONMENT=$3
+export API_URL=$2
+export ENVIRONMENT=$3
 docker-compose down
 docker-compose up
